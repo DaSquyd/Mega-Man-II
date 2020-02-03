e89594ff5e1475b57a28e6c3e99dad4a4ebbc5d5
DaSquyd <30611805+DaSquyd@users.noreply.github.com>
2020-02-03 17:21:06 -0500

    2/3/20

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameHandler : MonoBehaviour
+{
+    public static GameHandler handler;
+
+    public static bool FrameStepping = false;
+    public static bool Step { get; private set; }
+
+    private void Start()
+    {
+        handler = this;
+    }
+
+    private void FixedUpdate()
+    {
+        Step = false;
+
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Return))
+        {
+            FrameStepping = !FrameStepping;
+        }
+
+        if (!FrameStepping)
+            return;
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Step = true;
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,6 +26,13 @@ public class Player : MonoBehaviour
 
     // Public Vars
     public Vector2 Velocity;
+    public Vector2 Movement;
+
+    public float Speed;
+
+    public bool Stunned;
+    public bool OnGround;
+    public bool Jumping;
 
     public float floatHeight;
     public float liftForce;
@@ -49,42 +56,74 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (GameHandler.FrameStepping && !GameHandler.Step)
+            return;
+
+        Controls();
+
+        transform.Translate(Movement);
+        if (!OnGround)
+        {
+            Velocity += new Vector2(0f, gravityAcceleration);
+        }
+        else
+        {
+            Velocity = new Vector2(Velocity.x, 0f);
+        }
+
+        Move();
+    }
+
+    //https://gamefaqs.gamespot.com/boards/950625-mega-man-9/46926500
+
+    private void Controls()
+    {
+        if (Stunned)
+            return;
+        Velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, Velocity.y);
+
+        if (OnGround && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Velocity = new Vector2(Velocity.x, 4.875f);
+            OnGround = false;
+            Jumping = true;
+        }
+    }
+
+    private void Move()
+    {
         LayerMask groundMask = LayerMask.GetMask("Ground");
+
+        Movement = new Vector2();
+        OnGround = false;
 
         foreach (Corner c in corners)
         {
             Vector2 position = (Vector2)transform.position + c.position;
-            Vector2 directionH = c.directionX == DirectionH.LEFT ? new Vector2(Mathf.Min(Velocity.x, 0f), 0f) : new Vector2(Mathf.Max(Velocity.x, 0f), 0f);
-            Vector2 directionV = c.directionY == DirectionV.DOWN ? new Vector2(0f, Mathf.Min(Velocity.y, 0f)) : new Vector2(0f, Mathf.Max(Velocity.y, 0f));
+            Vector2 directionH = (c.directionX == DirectionH.LEFT ? new Vector2(Mathf.Min(Velocity.x, 0f), 0f) : new Vector2(Mathf.Max(Velocity.x, 0f), 0f)) * Time.fixedDeltaTime;
+            Vector2 directionV = (c.directionY == DirectionV.DOWN ? new Vector2(0f, Mathf.Min(Velocity.y, 0f)) : new Vector2(0f, Mathf.Max(Velocity.y, 0f))) * Time.fixedDeltaTime;
 
-            RaycastHit2D hitH = Physics2D.Raycast(position, directionH, directionH.x, groundMask);
-            RaycastHit2D hitV = Physics2D.Raycast(position, directionV, directionV.y, groundMask);
+            RaycastHit2D hitH = Physics2D.Raycast(position, directionH.normalized, directionH.magnitude, groundMask);
+            RaycastHit2D hitV = Physics2D.Raycast(position, directionV.normalized, directionV.magnitude, groundMask);
 
-            Debug.Log(directionV);
-            Debug.Log(directionV.y);
-
-            if (hitV.collider != null && hitV.distance != 0)
+            if (hitH.collider != null)
             {
-                directionV = new Vector2(0f, -hitV.distance);
+                directionH = hitH.normal * -hitH.distance;
+            }
+            if (hitV.collider != null)
+            {
+                directionV = hitV.normal * -hitV.distance;
             }
 
-            Debug.DrawRay(position, hitH.normal, Color.cyan, Time.fixedDeltaTime, false);
-            Debug.DrawRay(position, hitV.normal, Color.cyan, Time.fixedDeltaTime, false);
+            Debug.Log("Normal: " + directionV);
 
-            //Velocity = new Vector2(hitH.normal.x * hitH.distance, hitV.normal.y * hitV.distance);
+            Debug.DrawRay(position, directionH, Color.cyan, Time.fixedDeltaTime, false);
+            Debug.DrawRay(position, directionV, Color.cyan, Time.fixedDeltaTime, false);
 
-            // GRAVITY
-            //Velocity = new Vector2(Velocity.x, Velocity.y - (gravityAcceleration * Time.fixedDeltaTime));
+            Movement += directionH + directionV / 2f;
 
+            if (hitV.collider != null && hitV.distance == 0f && Velocity.y <= 0f)
+                OnGround = true;
         }
-
-        
-    }
-
-    private void Update()
-    {
-        // Move
-        if (Input.GetKeyDown(KeyCode.Space))
-            transform.position = transform.position + (Vector3)Velocity;
     }
 }
