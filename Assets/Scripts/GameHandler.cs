0e8469c2d1c53432ce3e2f8e9535f87e214b490f
David Jacquish <david.jacquish@septyr.com>
2020-02-05 00:54:57 -0500

    2/4/20

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -2,32 +2,83 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameHandler : MonoBehaviour
-{
+public class GameHandler : MonoBehaviour {
+    public struct VirtualController {
+        public VirtualButton A;
+        public VirtualButton B;
+        public VirtualButton Left;
+        public VirtualButton Right;
+        public VirtualButton Up;
+        public VirtualButton Down;
+        public VirtualButton Start;
+        public VirtualButton Select;
+        public VirtualButton FrameStep;
+    }
+
+    public struct VirtualButton {
+        public bool Value;
+        public bool Press;
+        public int IntValue {
+            get {
+                return Value ? 1 : 0;
+            }
+        }
+    }
+
+    public static VirtualController vc = new VirtualController();
+
     public static GameHandler handler;
 
     public static bool FrameStepping = false;
-    public static bool Step { get; private set; }
+    public static bool Step {
+        get; private set;
+    }
 
-    private void Start()
-    {
+    private void Start() {
         handler = this;
     }
 
-    private void FixedUpdate()
-    {
-        Step = false;
+    private void FixedUpdate() {
+        Controls();
 
-        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Return))
-        {
-            FrameStepping = !FrameStepping;
-        }
+        Step = false;
 
         if (!FrameStepping)
             return;
-        if (Input.GetKeyDown(KeyCode.Return))
-        {
+        if (vc.FrameStep.Press) {
             Step = true;
+        }
+    }
+
+    private void Controls() {
+        SetControlOutput(ref vc.A, KeyCode.X, "Jump");
+        SetControlOutput(ref vc.B, KeyCode.Z, "Shoot");
+        SetControlOutput(ref vc.Left, KeyCode.LeftArrow, "Horizontal", true);
+        SetControlOutput(ref vc.Right, KeyCode.RightArrow, "Horizontal", false);
+        SetControlOutput(ref vc.Up, KeyCode.UpArrow, "Vertical", true);
+        SetControlOutput(ref vc.Down, KeyCode.DownArrow, "Vertical", false);
+        SetControlOutput(ref vc.Start, KeyCode.Return, "Submit");
+        SetControlOutput(ref vc.Select, KeyCode.RightShift, "Cancel");
+        SetControlOutput(ref vc.FrameStep, KeyCode.Slash, "Step");
+    }
+
+    private void SetControlOutput(ref VirtualButton button, KeyCode key, string axis, bool negative = false) {
+        if (Input.GetKey(key) || Input.GetAxisRaw(axis) * (negative ? -1f : 1f) > 0f) {
+            if (button.Value) {
+                button.Press = false;
+            } else {
+                button.Value = true;
+                button.Press = true;
+            }
+        } else {
+            button.Value = false;
+            button.Press = false;
+        }
+    }
+
+    private void LateUpdate() {
+        if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Return)) {
+            FrameStepping = !FrameStepping;
         }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,24 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+public class Player : MonoBehaviour {
+    const float PIXELS = 32f;
+
     [System.Serializable()]
-    public struct Corner
-    {
+    public struct Corner {
         public Vector2 position;
         public DirectionH directionX;
         public DirectionV directionY;
     }
 
-    public enum DirectionH
-    {
+    public enum DirectionH {
         LEFT,
         RIGHT
     }
 
-    public enum DirectionV
-    {
+    public enum DirectionV {
         UP,
         DOWN
     }
@@ -28,7 +26,16 @@ public class Player : MonoBehaviour
     public Vector2 Velocity;
     public Vector2 Movement;
 
-    public float Speed;
+    public string GroundSpeed;
+    public string AccelSpeed;
+    public string decelSpeed;
+    public string AirSpeed;
+    public string JumpPower;
+    public string MinJump;
+    public string JumpCancelSpeed;
+    public string MaxFallSpeed;
+    public string ClimbSpeed;
+    public string Gravity;
 
     public bool Stunned;
     public bool OnGround;
@@ -38,92 +45,148 @@ public class Player : MonoBehaviour
     public float liftForce;
     public float damping;
 
-    [Tooltip("In m/s^2")]
-    public float gravityAcceleration;
-
     public Vector2 shape;
 
     public List<Corner> corners;
 
     // Private Vars
+    int oldDir;
+    int dirLock;
+    bool maxSpeed;
+    int accelCount;
+    int decelCount;
+
     BoxCollider2D col;
 
-    private void Start()
-    {
+    private void Start() {
         col = GetComponent<BoxCollider2D>();
+
+        Debug.Log("Ground Speed: " + Convert(GroundSpeed, false));
+        Debug.Log("Air Speed: " + Convert(AirSpeed, false));
+        Debug.Log("Jump Power: " + Convert(JumpPower, true));
+        Debug.Log("Min Jump: " + Convert(MinJump, false));
+        Debug.Log("Jump Cancel Speed: " + Convert(JumpCancelSpeed, false));
+        Debug.Log("Max Fall Speed: " + Convert(MaxFallSpeed, false));
+        Debug.Log("Climb Speed: " + Convert(ClimbSpeed, false));
+        Debug.Log("Gravity: " + Convert(Gravity, true));
     }
 
 
-    private void FixedUpdate()
-    {
-        if (GameHandler.FrameStepping && !GameHandler.Step)
-            return;
+    private void FixedUpdate() {
 
         Controls();
 
+        Move();
+
+        if (GameHandler.FrameStepping && !GameHandler.Step)
+            return;
+
         transform.Translate(Movement);
-        if (!OnGround)
-        {
-            Velocity += new Vector2(0f, gravityAcceleration);
-        }
-        else
-        {
+        if (!OnGround) {
+            Velocity -= new Vector2(0f, Convert(Gravity));
+        } else {
             Velocity = new Vector2(Velocity.x, 0f);
         }
 
-        Move();
     }
 
-    //https://gamefaqs.gamespot.com/boards/950625-mega-man-9/46926500
-
-    private void Controls()
-    {
+    private void Controls() {
         if (Stunned)
             return;
-        Velocity = new Vector2(Input.GetAxis("Horizontal") * Speed, Velocity.y);
 
-        if (OnGround && Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Velocity = new Vector2(Velocity.x, 4.875f);
+        float moveSpeed = Convert(OnGround ? GroundSpeed : AirSpeed);
+
+        int dir = GameHandler.vc.Right.IntValue - GameHandler.vc.Left.IntValue;
+
+        if (Mathf.Abs(dir) == 1 && dir != oldDir) {
+            if (accelCount < 7) {
+                moveSpeed = Convert(AccelSpeed);
+                accelCount++;
+            }
+        } else {
+            accelCount = 0;
+        }
+
+        if (dir == 0 && Mathf.Abs(oldDir) == 1) {
+            if (decelCount < 8) {
+                dir = oldDir;
+                moveSpeed = Convert(decelSpeed);
+                decelCount++;
+            }
+        } else {
+            decelCount = 0;
+        }
+
+        oldDir = dir;
+
+        Velocity = new Vector2(dir * moveSpeed, Velocity.y);
+
+        if (OnGround && GameHandler.vc.A.Press) {
+            Velocity = new Vector2(Velocity.x, Convert(JumpPower));
+            Debug.Log(Convert(JumpPower));
             OnGround = false;
             Jumping = true;
         }
     }
 
-    private void Move()
-    {
+    private void Move() {
+        LayerMask wallMask = LayerMask.GetMask("Wall");
         LayerMask groundMask = LayerMask.GetMask("Ground");
 
         Movement = new Vector2();
         OnGround = false;
 
-        foreach (Corner c in corners)
-        {
-            Vector2 position = (Vector2)transform.position + c.position;
-            Vector2 directionH = (c.directionX == DirectionH.LEFT ? new Vector2(Mathf.Min(Velocity.x, 0f), 0f) : new Vector2(Mathf.Max(Velocity.x, 0f), 0f)) * Time.fixedDeltaTime;
-            Vector2 directionV = (c.directionY == DirectionV.DOWN ? new Vector2(0f, Mathf.Min(Velocity.y, 0f)) : new Vector2(0f, Mathf.Max(Velocity.y, 0f))) * Time.fixedDeltaTime;
+        foreach (Corner c in corners) {
+            Vector2 position = (Vector2) transform.position + c.position;
+            Vector2 directionH = c.directionX == DirectionH.LEFT ? new Vector2(Mathf.Min(Velocity.x, 0f), 0f) : new Vector2(Mathf.Max(Velocity.x, 0f), 0f);
+            Vector2 directionV = c.directionY == DirectionV.DOWN ? new Vector2(0f, Mathf.Min(Velocity.y, 0f)) : new Vector2(0f, Mathf.Max(Velocity.y, 0f));
 
-            RaycastHit2D hitH = Physics2D.Raycast(position, directionH.normalized, directionH.magnitude, groundMask);
-            RaycastHit2D hitV = Physics2D.Raycast(position, directionV.normalized, directionV.magnitude, groundMask);
+            if (directionV.y < -Convert(MaxFallSpeed) / 2f)
+                directionV = new Vector2(0f, -Convert(MaxFallSpeed) / 2f);
 
-            if (hitH.collider != null)
-            {
+            RaycastHit2D hitH = Physics2D.Raycast(position, directionH.normalized, directionH.magnitude * 2f, wallMask);
+            RaycastHit2D hitV = Physics2D.Raycast(position, directionV.normalized, directionV.magnitude * 2f, groundMask);
+
+            if (hitH.collider != null) {
                 directionH = hitH.normal * -hitH.distance;
             }
-            if (hitV.collider != null)
-            {
-                directionV = hitV.normal * -hitV.distance;
+            if (hitV.collider != null) {
+                directionV = hitV.normal * -hitV.distance / 2f;
             }
 
-            Debug.Log("Normal: " + directionV);
+            Debug.Log(directionV);
 
-            Debug.DrawRay(position, directionH, Color.cyan, Time.fixedDeltaTime, false);
-            Debug.DrawRay(position, directionV, Color.cyan, Time.fixedDeltaTime, false);
+            Movement += (directionH + directionV);
 
-            Movement += directionH + directionV / 2f;
+            if (Movement.y > Convert(MinJump) && !GameHandler.vc.A.Value && Jumping) {
+                Debug.Log("Jump Cancelled");
+                Movement = new Vector2(Movement.x, Convert(JumpCancelSpeed));
+                Velocity = new Vector2(Velocity.x, Convert(JumpCancelSpeed));
+                Jumping = false;
+            }
 
-            if (hitV.collider != null && hitV.distance == 0f && Velocity.y <= 0f)
+            if (Movement.y < 0f)
+                Jumping = false;
+
+            if (hitV.collider != null && hitV.distance == 0f && Velocity.y <= 0f) {
                 OnGround = true;
+            }
+
+            
+            Debug.DrawRay(position, new Vector2(Movement.x, 0f), Color.cyan, Time.fixedDeltaTime, false);
+            Debug.DrawRay(position, new Vector2(0f, Movement.y), Color.red, Time.fixedDeltaTime, false);
         }
+    }
+
+    private float Convert(string hex, bool divideByPixels = true) {
+        string[] segments = hex.Split('.');
+
+        float first = int.Parse(segments[0], System.Globalization.NumberStyles.HexNumber);
+        float last = int.Parse(segments[1], System.Globalization.NumberStyles.HexNumber) / 256f;
+
+        if (divideByPixels)
+            return (first + last) / PIXELS;
+        else
+            return first + last;
     }
 }
